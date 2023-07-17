fc8455d9e37e652254736a2a65060dca34130cc2
priyankachavan25 <130668193+priyankachavan25@users.noreply.github.com>
2023-07-17 13:05:53 +0530

    Update tree basics along with files.c

--- tree basics along with files.c
+++ tree basics along with files.c
@@ -101,6 +101,4 @@ int main() {
 }
 
 
-/*In this code, the tree operations (insertion, inorder traversal, preorder traversal, postorder traversal) are implemented in functions that take an additional FILE* parameter. The random numbers are generated and saved to a file named random.txt using the fprintf function. The numbers from the file are then inserted into the tree. The results of each operation are written to separate files (inorder.txt, preorder.txt, postorder.txt).
 
-Make sure to create an empty file named random.txt in the same directory as the code*/
